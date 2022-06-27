e5892478a63a37389a0bdf42d62b13acce43fe5b
Noble Dillon <ndillon@pr-g2.kzn.21-school.ru>
2022-06-27 15:28:22 +0300

    cub start

---
+++ includes/cub.h
@@ -0,0 +1,12 @@
+#ifndef CUB_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <errno.h>
+# include "mlx.h"
+# include "libft.h"
+# include "get_next_line.h"
+
+#endif
